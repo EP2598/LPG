2d091d8aa66feb54c8b0996ac37bc126d60e8495
ClasherGeek77 <52255311+ClasherGeek77@users.noreply.github.com>
2019-06-27 21:30:34 +0700

    function needs fixed

--- PemrogramanDasar_Chapter5_MengurutkanHarga.cpp
+++ PemrogramanDasar_Chapter5_MengurutkanHarga.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 void mengurutkanHarga(int n, int harga[]){
-	
+	int temp;
+	for(int i=0; i<n; i++){
+		for(int j=i+1; j<n; j++){
+			if(harga[i]>harga[j]){
+				temp=harga[i];
+				harga[i]=harga[j];
+				harga[j]=temp;
+				cout << i+1 << " " << j+1 << endl;
+			}
+		}
+	}
 }
 int main(){
 	int n;
