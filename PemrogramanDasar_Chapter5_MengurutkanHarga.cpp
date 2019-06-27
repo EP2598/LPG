88e9657e7dd1dca25c6f5ece5a65e3b3ce78a196
ClasherGeek77 <52255311+ClasherGeek77@users.noreply.github.com>
2019-06-27 21:17:17 +0700

    function needs fixed

---
+++ PemrogramanDasar_Chapter5_MengurutkanHarga.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void mengurutkanHarga(int n, int harga[]){
+	
+}
+int main(){
+	int n;
+	cin >> n;
+	int harga[n];
+	for (int i=0;i<n;i++){
+		cin>>harga[n];
+	}
+	mengurutkanHarga(n,harga);
+}
