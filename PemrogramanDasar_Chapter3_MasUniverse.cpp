37a610a81b4bfafa956bd7f216e016515dcb4063
ClasherGeek77 <52255311+ClasherGeek77@users.noreply.github.com>
2019-06-27 20:20:01 +0700

    fixed algorithm

--- PemrogramanDasar_Chapter3_MasUniverse.cpp
+++ PemrogramanDasar_Chapter3_MasUniverse.cpp
@@ -2,14 +2,22 @@
 using namespace std;
 
 int main(){
-	int n,x;
-	int p;
+/*Deskripsi
+Format Masukan
+Baris pertama berisi sebuah bilangan bulat N, banyak peserta Mas Universe.
+
+Baris kedua berisi N buah bilangan bulat Pi, tingkat kekuatan tiap peserta.
+
+Format Keluaran
+Satu baris berisi sebuah bilangan bulat, tingkat kekuatan Mas Universe yang pertama.*/
+	int n, i, x=0;
 	cin >> n;
-	for(int i=0;i<n;i++){
-		cin >> p;
-		if(i=0){
-			x=p;
+	int kekuatan[n];
+	for (i=0; i<n; i++){
+		cin >> kekuatan[i];
+		if (x<kekuatan[i]){
+			x=kekuatan[i];
 		}
 	}
-	cout << x << endl;
+	cout << x << "\n";
 }
