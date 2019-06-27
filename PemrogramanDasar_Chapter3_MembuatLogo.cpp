6c262ba43950145c5af51aa58ecade2644c397a2
yuriko192 <ivanwijaya19@yahoo.co.id>
2019-06-27 14:23:35 +0700

    added ch3-Membuat Logo

---
+++ PemrogramanDasar_Chapter3_MembuatLogo.cpp
@@ -0,0 +1,50 @@
+/*
+7 7 2
+*******
+*******
+**.....
+**.....
+**.....
+*******
+*******
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cin >> y >> x >> z;
+    for (int i = 0; i < z; i++)
+    {
+        for (int ii = 0; ii < x; ii++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    } //bagian atas
+
+    for (int i = 0; i < y - z * 2; i++)
+    {
+        for (int ii = 0; ii < z; ii++)
+        {
+            cout << '*';
+        }
+        for (int ii = 0; ii < x - z; ii++)
+        {
+            cout << '.';
+        }
+        cout << endl;
+    } //bagian tengah
+
+    for (int i = 0; i < z; i++)
+    {
+        for (int ii = 0; ii < x; ii++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    } //bagian bawah
+
+    return 0;
+}
