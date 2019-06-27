a901e6c74592127ec5487b2c8ab035f966c6d737
El <25134162+EP2598@users.noreply.github.com>
2019-06-27 19:05:54 +0700

    Using inequality of triangle theorem

--- PemrogramanDasar_Chapter2_Segitiga.cpp
+++ PemrogramanDasar_Chapter2_Segitiga.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  short int a, b, c;
-
-  cin >> a >> b >> c;
-
-  if (a + b > c && a + c > b && b + c > a)
-  {
-    cout << "segitiga\n";
-  }
-  else
-  {
-    cout << "bukan segitiga\n";
-  }
+int main(){
+	int a,b,c;
+	cin >> a >> b >> c;
+	//Inequality of Triangle : If a+b>c && a+c>b && b+c>a then triangle.
+	if(a+b>c&&a+c>b&&b+c>a)
+		cout << "segitiga" << endl;
+	else
+		cout << "bukan segitiga" << endl;
 }
