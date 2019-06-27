70a014f8f57f8b1b105df123485460130ef9eac8
yuriko192 <ivanwijaya19@yahoo.co.id>
2019-06-27 22:46:08 +0700

    fixed runtime error (flatten)+ output format

--- PemrogramanDasar_Chapter4_Transpose.cpp
+++ PemrogramanDasar_Chapter4_Transpose.cpp
@@ -5,22 +5,21 @@ int main(){
   int i, j, m, n;
   cin >> m;
   cin >> n;
-  int matriks[m][n], transpose[m][n];
+  int *matriks = new int[m*n], *transpose= new int[m*n];
   for (i = 0; i < m; i++){
     for (j = 0; j < n; j++){
-      cin  >> matriks[i][j];
+      cin  >> matriks[i*n+j];
     }
   }
   for (i = 0; i < m; i++){
     for (j = 0; j < n; j++){
-      transpose[j][i] = matriks[i][j];
+      transpose[j*m+i] = matriks[i*n+j];
     }
   }
   for (i = 0; i < n; i++){
-    for (j = 0; j < m; j++){
-      cout << transpose[i][j] << " ";
+    for (j = 0; j < m-1; j++){
+      cout << transpose[i*m+j] << " ";
     }
-    cout << "\n";
+    cout<<transpose[i*m+m-1]<<endl;
   }
-	cout << "\n";
 }
