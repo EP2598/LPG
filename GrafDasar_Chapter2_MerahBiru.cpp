2da2166bfb86f0d964144e91b2e6f1c714be8138
yuriko192 <ivanwijaya19@yahoo.co.id>
2019-06-29 22:09:05 +0700

    improved logic, (time limit)

--- GrafDasar_Chapter2_MerahBiru.cpp
+++ GrafDasar_Chapter2_MerahBiru.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 int n;
 
-void checkarr(vector<vector<bool>> arr)
+void checkarr(vector<vector<int>> arr )
 {
 	for (int i = 0; i < n; i++)
 	{
-		for (int ii = 0; ii < n; ii++)
-		{
-			cout << (bool)arr[i][ii];
-		}
+		for (auto ii = arr[i].begin(); ii != arr[i].end(); ++ii)
+			cout << *ii << " ";
 		cout << endl;
 	}
 }
-bool dfs(vector<vector<bool>> graph, int vertex, vector<int> color, vector<bool> visited, int curr_col)
+bool dfs(vector<vector<int>> graph, int vertex, vector<int> color, vector<bool> visited, int curr_col)
 {
 	if (!visited[vertex])
 	{
@@ -22,19 +20,15 @@ bool dfs(vector<vector<bool>> graph, int vertex, vector<int> color, vector<bool>
 		color[vertex] = curr_col;
 		bool temp = true;
 		//cout << "Checking vertex: " << vertex << endl;
-		for (int i = 0; i < n; i++)
+		for (auto i = graph[vertex].begin(); i != graph[vertex].end(); ++i)
 		{
-			if (i == vertex)
-				continue;
-			if (graph[vertex][i])
+			//cout << "    going to: " << i << " with expected color: " << (curr_col + 1) % 2 << endl;
+			temp = temp && dfs(graph, *i, color, visited, (curr_col + 1) % 2);
+			if (!temp)
 			{
-				//cout << "    going to: " << i << " with expected color: " << (curr_col + 1) % 2 << endl;
-				temp = temp && dfs(graph, i, color, visited, (curr_col + 1) % 2);
-				if (!temp)
-				{
-					return false;
-				}
+				return false;
 			}
+
 		}
 		return temp;
 	}
@@ -49,21 +43,20 @@ int main()
 {
 	int m;
 	cin >> n >> m;
-	vector<vector<bool>> arr(n,vector<bool>(n));
+	vector<vector<int>> arr(n);
 	vector<bool> visit(n);
 	vector<int> color(n, -1);
-	
+
 	for (int i = 0; i < m; i++)
 	{
 		int a, b;
 		cin >> a >> b;
 		a--;
 		b--;
-		arr[a][b] = true;
-		arr[b][a] = true;
+		arr[a].push_back(b);
+		arr[b].push_back(a);
 	}
 	//checkarr(arr);
-	//dfs(Graph,vertex,color,visited)
 
 	if (dfs(arr, 0, color, visit, 0))
 	{
