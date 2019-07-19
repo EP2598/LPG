1c029f18c16645df9ee6574beadde4bd3d465ac1
ClasherGeek77 <52255311+ClasherGeek77@users.noreply.github.com>
2019-07-19 11:41:45 +0700

    optimization needed

--- Coder Class Week 1/BaskomMania.cpp
+++ Coder Class Week 1/BaskomMania.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
+int func(int m, int n, int k, int temp, int a[]){
+			int hasil;
+			bool lanjut=1;
+			int ii=0;
+			int j=0;
+			while(j==0){
+				m=m+n*k-temp;
+				if(m<=0){
+				j=ii;
+				m=m-n*k+temp;
+				while(lanjut){
+				m=m+k;
+				if(m<a[j%n]){
+					continue;
+				}
+				m=m-a[j%n];
+				j++;
+				if(m==0){
+					hasil=j;
+					lanjut=0;
+				}
+				}
+				}
+			ii+=n;
+			}
+	return hasil;
+}
+
 int main(){
-	int t;
+	short int t;
 	int m,n,k,temp=0;
 	cin >> t;
 	int hasil[t];
@@ -12,22 +40,14 @@ int main(){
 			cin >> a[ii];
 			temp+=a[ii];
 		}
-		hasil[i]=-1;
-		if((k*n)-temp<0){
-			bool lanjut=1;
-			int j=0;
-			
-			while(lanjut){
-				m=m+k-a[j];
-				if(m<0){
-					hasil[i]=j-1;
-					lanjut=0;
-				}
-				j=(j+1)%n;
-			}
-		}
+	hasil[i]=-1;
+	if((k*n)-temp<=0){
+	hasil[i]=func(m,n,k,temp,a);;
+	}
+	temp=0;
 	}
 	for(int i=0;i<t;i++){
 		cout << hasil[i] << endl;
-	}	
+	}
+	return 0;
 }
